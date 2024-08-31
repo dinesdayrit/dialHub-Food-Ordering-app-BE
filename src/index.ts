5dc11fd37aa87a587bfff2a701b73046094e62d6
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-08-31 15:42:22 +0800

    feat: add user schema

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => console.log("Connected to DB"));
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   res.json({ message: "Hello!" });
 });
 
