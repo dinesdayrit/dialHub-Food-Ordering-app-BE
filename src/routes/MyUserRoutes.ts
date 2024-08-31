25a7df3a57c16425dcbe4a48b052948d5b7887fa
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-08-31 22:15:32 +0800

    add jwtParse middleware

--- src/routes/MyUserRoutes.ts
+++ src/routes/MyUserRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import MyUserController from "../controller/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 
 const router = express.Router();
 
 // api//my/user
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
-router.put("/", jwtCheck, MyUserController.updateCurrentUser);
+router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
 export default router;
